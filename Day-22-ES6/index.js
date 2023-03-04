e540209546f7746494dd2b0c9163030f1e523513
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-03-04 23:50:34 +0600

    day -22

--- Day-22-ES6/index.js
+++ Day-22-ES6/index.js
@@ -484,6 +484,12 @@ Waiting:Destructuring on list should be used.
 */
 
 
+function removeFirstTwo(list){
+  const [ , , ...shorterList] = list;
+  return shorterList;
+}
+const source = [1,2,3,4,5,6,7,8,9,10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
 
 
 
@@ -511,7 +517,6 @@ Waiting:stats should be an object.
 Waiting:half(stats) should be 28.015
 Waiting:Destructuring should be used.
 Waiting:Destructured parameter should be used.
-
 */
 
 
