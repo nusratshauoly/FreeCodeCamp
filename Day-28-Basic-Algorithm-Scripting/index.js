544ab4d18e14d289d339a63eb77bfc5f32886190
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-05-06 21:40:53 +0600

    revise

--- Day-28-Basic-Algorithm-Scripting/index.js
+++ Day-28-Basic-Algorithm-Scripting/index.js
@@ -181,3 +181,4 @@ Waiting:chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1,
 Waiting:chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
 */
 
+
